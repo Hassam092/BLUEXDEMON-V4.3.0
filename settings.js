57ab7e9a7271beed70f1b5a26fd4930a3e965dfd
Hassam092 <canikissu092@gmail.com>
2024-12-21 15:26:29 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const version = require("@whiskeysockets/baileys/package.json").version
 //Input number for pair code
-global.pairingNumber = "2349019529423"
+global.pairingNumber = "923269290657"
 
-global.botNumber = ["2349019529423"]
+global.botNumber = ["923269290657"]
 
 global.anticall = false
 
